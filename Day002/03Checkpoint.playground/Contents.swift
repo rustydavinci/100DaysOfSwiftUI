7ce7014c0b3b23d8825b53c4f1ab6dcea0f95450
David Ossa <rustydavinci@icloud.com>
2024-10-03 18:07:52 +0100

    Add playground files for day 2

---
+++ Day002/03Checkpoint.playground/Contents.swift
@@ -0,0 +1,6 @@
+import Cocoa
+
+let temperatureInCelsius = 25.0
+let temperatureInFarenheit = temperatureInCelsius * 9 / 5 + 32
+
+print("\(temperatureInCelsius)°C is equivalent to \(temperatureInFarenheit)°F")
